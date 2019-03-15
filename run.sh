6f8b7b5058636bf702cb2c267c99faf922beaac0
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-03-15 20:29:15 +0100

    jenkins configuration as code

---
+++ run.sh
@@ -0,0 +1,23 @@
+BASEDIR=$(readlink -f $(dirname "$0") )
+ENV_FILE=${BASEDIR}/env
+
+[ -f ${ENV_FILE} ] || {
+    echo "Configuration file missing: $ENV_FILE"
+    exit 1
+}
+
+. $ENV_FILE
+
+docker run \
+  --name jenkins \
+  -d \
+  --restart=unless-stopped \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -u root \
+  -v /var/opt/jenkins:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v ${BASEDIR}/config:/var/jenkins_home/casc_configs \
+  -e CASC_JENKINS_CONFIG=/var/jenkins_home/casc_configs \
+  -e JENKINS_URL=http://${HOSTNAME}:8080/ \
+  labasc/javaci-jenkins
